3a4d3f8dace3581eeb62234de6191508fe542499
monandkey <satoru070505@gmail.com>
2021-12-19 05:11:14 +0000

    [update] private key
    Support for public key with passphrase.

--- pkg/ssh/auth.go
+++ pkg/ssh/auth.go
@@ -17,16 +17,22 @@ func sshPasswordAuthorization(userName string, password string) (*ssh.ClientConf
 	return config, nil
 }
 
-func sshPublicKeyAuthorization(userName string, publicKey string) (*ssh.ClientConfig, error) {
+func sshPublicKeyAuthorization(userName string, publicKey string, passphrase string) (*ssh.ClientConfig, error) {
 	var sshconfig *ssh.ClientConfig
 	buf, err := ioutil.ReadFile(publicKey)
 	if err != nil {
 		return sshconfig, err
 	}
 
-	key, err := ssh.ParsePrivateKey(buf)
+	// This function will not work properly if there is a passphrase.
+	// If you have a passphrase, please use "ParsePrivateKeyWithPassphrase".
+	var key ssh.Signer
+	key, err = ssh.ParsePrivateKey(buf)
 	if err != nil {
-		return sshconfig, err
+		key, err = sshPublicKeyAuthorizationWithPassphrase(buf, []byte(passphrase))
+		if err != nil {
+			return sshconfig, err
+		}
 	}
 
 	config := &ssh.ClientConfig{
@@ -36,4 +42,13 @@ func sshPublicKeyAuthorization(userName string, publicKey string) (*ssh.ClientCo
 		},
 	}
 	return config, nil
+}
+
+func sshPublicKeyAuthorizationWithPassphrase(buf []byte, passphrase []byte) (ssh.Signer, error) {
+	var signer ssh.Signer
+	signer, err := ssh.ParsePrivateKeyWithPassphrase(buf, passphrase)
+	if err != nil {
+		return signer, err
+	}
+	return signer, nil
 }
--- pkg/ssh/auth_test.go
+++ pkg/ssh/auth_test.go
@@ -48,3 +48,68 @@ func Test_sshPasswordAuthorization(t *testing.T) {
 		})
 	}
 }
+
+func Test_sshPublicKeyAuthorization(t *testing.T) {
+	var (
+		username  = "root"
+		publicKey = "/root/.ssh/id_rsa"
+	)
+
+	type args struct {
+		userName   string
+		publicKey  string
+		passphrase string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *ssh.ClientConfig
+		wantErr bool
+	}{
+		{
+			name: "Normal Test",
+			args: args{
+				userName:   username,
+				publicKey:  publicKey,
+				passphrase: "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Normal passphrase Test",
+			args: args{
+				userName:   username,
+				publicKey:  "/root/.ssh/id_test",
+				passphrase: "test",
+			},
+			wantErr: false,
+		},
+		{
+			name: "failure Test",
+			args: args{
+				userName:   username,
+				publicKey:  "/root/.ssh/id_abc",
+				passphrase: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Failure passphrase Test",
+			args: args{
+				userName:   username,
+				publicKey:  "/root/.ssh/id_test",
+				passphrase: "testt",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := sshPublicKeyAuthorization(tt.args.userName, tt.args.publicKey, tt.args.passphrase)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("sshPublicKeyAuthorization() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
